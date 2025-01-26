5b0ec5c105d31bb684e1da90849aeed8927376a7
jibolashittubolu <jibolashittubolu@gmail.com>
2025-01-26 20:03:30 +0100

    committing the modifications

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          To learn Front-End Development and to earn!
         </p>
         <a
           className="App-link"
-          href="https://react.dev"
+          href="https://wikipedia.com"
           target="_blank"
           rel="noopener noreferrer"
         >
