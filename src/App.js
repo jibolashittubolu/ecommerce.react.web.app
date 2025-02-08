06a53c95a0620c14ceb01007433ce163e0ee2589
jibolashittubolu <jibolashittubolu@gmail.com>
2025-02-09 00:05:48 +0100

    added counter-click-feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; //jpg,jpeg, png, gif, bitmap, svg
 import './App.css';
+import { useState } from 'react';
 
-// TASKS
-// 1. Change the background to lime
-// 2. Add a new paragraph stating your interest in front-end development
-// 3. Change the link text to your github profile name
-// 4. Change the link destination to point to your profile 
+
 function App() {
+  const [value, setValue] = useState(0)
+  // const [condition, setCondition] = useState("good")
+
+  //collect the user's preferred initial state
+  //set the initial state to memory
+  //anytime you restart the app, it should fetch the initial state from memory
+
   return (
-    <div className="App">
-      <header className="App-header" >
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          To learn Front-End Development and to earn!
-        </p>
-        <a
-          className="App-link"
-          href="https://wikipedia.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactyyy 
-        </a>
+    <div className='app'>
+      <header className='app-header'>
+        <span> This is a sample app that shows us how to do basic operations in React.js  </span>
       </header>
+      <section className='display-value-container'>
+        <div className='display-value'>
+          {value}
+        </div>
+        <div className='mathematical-buttons'>
+          <button 
+          className='subtract-button'
+          onClick={()=>{
+            setValue(prev => {
+              return prev - 1
+            })
+          }}
+          >-</button>
+          {/* <div>sample div</div> */}
+          <button 
+          className='add-button' 
+          onClick={()=>{
+            setValue(previo => {
+              return previo + 1
+            })
+          }}
+          >+</button>
+        </div>
+        {/* <SchoolList state={}  setSchoolList/> */}
+      </section>
+      <section className='initial-state-collector-container'>
+          {/* <label htmlFor=''></label> */}
+          <input
+          className='initial-state-input-box' 
+          type="number" 
+          placeholder='choose your initial number'/>
+          <button  >Ok</button>
+      </section>
+      
     </div>
   );
 }
